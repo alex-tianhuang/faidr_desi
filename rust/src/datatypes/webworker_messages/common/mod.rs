d888ea40f2c85203d5734e7134993f000808a560
alex-dsci <alex-dsci@github.com>
2026-03-27 19:07:23 -0400

    hand roll Deserialize impl for tagged enum

--- rust/src/datatypes/webworker_messages/common/mod.rs
+++ rust/src/datatypes/webworker_messages/common/mod.rs
@@ -1,6 +1,6 @@
 //! Common message types for the webworker backend.
 use crate::adapters::JsValuePreserved;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, de::DeserializeOwned};
 use serde_wasm_bindgen::from_value;
 use tsify::Tsify;
 use wasm_bindgen::{JsValue, prelude::wasm_bindgen};
@@ -19,7 +19,7 @@ pub struct Request<T = JsValuePreserved> {
 /// Response (or `RecvMessage`) type.
 ///
 /// Sent from backend to frontend.
-#[derive(Tsify, Deserialize, Serialize)]
+#[derive(Tsify, Serialize)]
 #[tsify(into_wasm_abi)]
 #[serde(tag = "case", rename_all = "snake_case")]
 pub enum Response<S, T = JsValuePreserved> {
@@ -27,15 +27,14 @@ pub enum Response<S, T = JsValuePreserved> {
     Close { data: T },
     Error { reason: S },
 }
-
 /// Response data type for batched backend.
-/// 
+///
 /// See also [`crate::TaskSpawner`].
 #[derive(Tsify, Deserialize, Serialize)]
 pub struct ResponsePayloadWithWorkerID {
     #[serde(rename = "workerID")]
     pub worker_id: u32,
-    pub data: JsValuePreserved
+    pub data: JsValuePreserved,
 }
 /// Get a `connID` field from a request object.
 pub fn get_connection_id(request: JsValue) -> Result<String, serde_wasm_bindgen::Error> {
@@ -43,7 +42,58 @@ pub fn get_connection_id(request: JsValue) -> Result<String, serde_wasm_bindgen:
     #[derive(Deserialize)]
     struct ConnectionID {
         #[serde(rename = "connID")]
-        conn_id: String
+        conn_id: String,
     }
     from_value::<ConnectionID>(request).map(|ConnectionID { conn_id }| conn_id)
+}
+// serde derive + tags + JsValuePreserved doesn't work together
+// according to Claude Sonnet 4.6, so handrolling deserialize here
+impl<'de, S, T> Deserialize<'de> for Response<S, T>
+where
+    S: DeserializeOwned,
+    T: DeserializeOwned,
+{
+    fn deserialize<D>(de: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let value: JsValue = serde_wasm_bindgen::preserve::deserialize(de)?;
+        #[derive(Deserialize)]
+        #[serde(rename_all = "snake_case")]
+        enum ResponseCase {
+            Yield,
+            Close,
+            Error,
+        }
+        #[derive(Deserialize)]
+        struct Response_ {
+            case: ResponseCase,
+        }
+        let Response_ { case } =
+            from_value::<Response_>(value.clone()).map_err(serde::de::Error::custom)?;
+        match case {
+            ResponseCase::Close | ResponseCase::Yield => {
+                #[derive(Deserialize)]
+                struct Response_<T> {
+                    data: T,
+                }
+                let Response_ { data } =
+                    from_value::<Response_<T>>(value).map_err(serde::de::Error::custom)?;
+                if matches!(case, ResponseCase::Yield) {
+                    Ok(Response::Yield { data })
+                } else {
+                    Ok(Response::Close { data })
+                }
+            }
+            ResponseCase::Error => {
+                #[derive(Deserialize)]
+                struct Response_<S> {
+                    reason: S,
+                }
+                let Response_ { reason } =
+                    from_value::<Response_<S>>(value).map_err(serde::de::Error::custom)?;
+                Ok(Response::Error { reason })
+            }
+        }
+    }
 }
