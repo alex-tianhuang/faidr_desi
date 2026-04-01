bd5e60fc0cecc79c76c1f7ae1c110f4ed578e4fb
alex-dsci <alex-dsci@github.com>
2026-04-01 18:34:19 -0400

    update timestamp every time a new seed is requested

--- js/src/components/rngPicker.tsx
+++ js/src/components/rngPicker.tsx
@@ -3,12 +3,12 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 
 export default function RngPicker(props: {
-    timestamp: number;
+    reqTimestampState: [number, (_: number) => void];
     disabled: boolean;
     setRngSeed: (_: number) => void;
+    rngHintState: [string, (_: string) => void]
 }) {
-    const { disabled, setRngSeed, timestamp } = props
-    const [rngHint, setRngHint] = useState("");
+    const { disabled, setRngSeed, reqTimestampState: [timestamp, setTimestamp], rngHintState: [rngHint, setRngHint] } = props
 
     useEffect(() => {
         const hint = Number.parseInt(rngHint);
@@ -19,7 +19,7 @@ export default function RngPicker(props: {
     return (<div className="flex flex-row gap-3 items-center">
         <Input
             disabled={disabled}
-            placeholder="Enter a seed for generating a random sequence (optional)"
+            placeholder="Enter a number between 0 and 2 ^ 32"
             value={rngHint}
             onChange={(e) => {
                 const rngHint = e.target.value;
@@ -27,6 +27,9 @@ export default function RngPicker(props: {
             }}
         />
         <span>OR</span>
-        <Button disabled={disabled || rngHint.length > 0} onClick={() => setRngHint(`${timestamp}`)}>Generate a fixed seed</Button>
+        <Button disabled={disabled || rngHint.length > 0} onClick={() => {
+            setRngHint(`${timestamp % (2 ** 32)}`);
+            setTimestamp(Date.now())
+        }}>Generate a fixed seed</Button>
     </div>);
 }
