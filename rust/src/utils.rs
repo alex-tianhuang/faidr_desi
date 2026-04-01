4b2bb51be4ba450595fe1b617c0cc9c118d88866
alex-dsci <alex-dsci@github.com>
2026-04-01 15:43:29 -0400

    return relevant span with parsing

--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -1,37 +1,69 @@
-use crate::datatypes::aa_canonical_str;
+use crate::{adapters::JsValuePreserved, datatypes::aa_canonical_str};
+use serde::Serialize;
+use tsify::Tsify;
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::js_sys::Error;
 
+/// Return type of [`parse_text_as_sequence`].
+#[derive(Tsify, Serialize)]
+#[tsify(into_wasm_abi)]
+#[serde(tag = "case", rename_all = "kebab-case")]
+pub enum ParsedSequence {
+    #[serde(rename_all = "camelCase")]
+    Ok {
+        sequence: String,
+        relevant_span: [u32; 2],
+    },
+    #[serde(rename_all = "camelCase")]
+    Error {
+        error: JsValuePreserved<Error>,
+        relevant_span: [u32; 2],
+    },
+}
 /// Given some text from a text area, parse a sequence from it.
 #[wasm_bindgen(js_name = parseTextAsSequence)]
-pub fn parse_text_as_sequence(text: String) -> Result<String, Error> {
+pub fn parse_text_as_sequence(text: String) -> ParsedSequence {
     let mut text = text.into_bytes();
-    let seq = if text.starts_with(b">") {
-        let n = end_of_current_header(&text).ok_or_else(|| Error::new("Text area contains only header."))?;
-        let rest = &mut text[n..];
+    let (relevant_span, seq) = if text.starts_with(b">") {
+        let Some(m) = end_of_current_header(&text) else {
+            return ParsedSequence::Error {
+                error: JsValuePreserved::new(Error::new("Text area contains only header")),
+                relevant_span: [0, 0],
+            };
+        };
+        let rest = &mut text[m..];
         let n = next_start_of_header(&rest).unwrap_or(rest.len());
-        &mut rest[..n]
+        (m..m + n, &mut rest[..n])
     } else {
-        &mut text
+        (0..text.len(), &mut *text)
     };
-    let seq = aa_canonical_str::join_multiline(seq)
-        .map_err(|e| Error::new(&format!("Could not parse pasted text as sequence: {}", e)))?;
-    Ok(seq.as_str().to_string())
-}
-/// Given some file text, parse the first sequence from it.
-/// 
-/// The only part of this that assumes this to be text from
-/// a file are the error messages.
-#[wasm_bindgen(js_name = parseFirstSequenceOfFasta)]
-pub fn parse_first_sequence_of_fasta(file_text: String) -> Result<String, Error> {
-    let mut text = file_text.into_bytes();
-    let n = end_of_current_header(&text).ok_or_else(|| Error::new("File is empty."))?;
-    let rest = &mut text[..n];
-    let n = next_start_of_header(&rest).unwrap_or(rest.len());
-    let seq = &mut rest[..n];
-    let seq = aa_canonical_str::join_multiline(seq)
-        .map_err(|e| Error::new(&format!("Could not parse sequence after first header: {}", e)))?;
-    Ok(seq.as_str().to_string())
+    let mut seq_buffer = Vec::from(seq);
+    match aa_canonical_str::join_multiline(&mut seq_buffer) {
+        Ok(seq) => ParsedSequence::Ok {
+            sequence: seq.as_str().to_string(),
+            relevant_span: [relevant_span.start as u32, relevant_span.end as u32],
+        },
+        Err(e) => {
+            let pos = relevant_span.start + e.at;
+            let line_no = text[..pos].iter().filter(|b| **b == b'\n').count() + 1;
+            let col_no = match text[..pos].iter().rev().position(|b| *b == b'\n') {
+                Some(n) => {
+                    let last_line_pos = pos - 1 - n;
+                    debug_assert_eq!(text[last_line_pos], b'\n');
+                    debug_assert!(text[last_line_pos+1..pos].iter().all(|b| *b != b'\n'));
+                    n + 1
+                }
+                None => pos + 1,
+            };
+            ParsedSequence::Error {
+                error: JsValuePreserved::new(Error::new(&format!(
+                    "Cannot parse `{}` as a capitalized aminoacid at line {}, column {}",
+                    e.ch, line_no, col_no
+                ))),
+                relevant_span: [relevant_span.start as u32, relevant_span.end as u32],
+            }
+        }
+    }
 }
 
 /// Find the index of the next newline from the beginning of this slice.
