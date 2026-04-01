49c95ba273631f0a968cb99661fae7f1e13ff41b
alex-dsci <alex-dsci@github.com>
2026-04-01 15:42:51 -0400

    add destructuring to forward_request

--- rust/src/servers/non_blocking/forward.rs
+++ rust/src/servers/non_blocking/forward.rs
@@ -1,9 +1,10 @@
+use serde_wasm_bindgen::from_value;
 use wasm_bindgen::JsValue;
 use wasm_bindgen_futures::future_to_promise;
 
 use crate::{
     ResponsePayloadWithWorkerID, TaskSpawner,
-    adapters::{JsValuePreserved, SenderHandle},
+    adapters::SenderHandle,
     datatypes::{Request, Response},
 };
 
@@ -11,14 +12,16 @@ use crate::{
 ///
 /// All responses from the given job are propagated
 /// back to the [`SenderHandle`].
+///
+/// This function panics if the `request` does not have a `data` field.
+/// Use [`crate::datatypes::webworker_messages::non_blocking::request_is_forwardable`] to validate this.
 pub fn forward_request(
     request: JsValue,
     task_spawner: &TaskSpawner,
     sender: SenderHandle,
 ) -> impl Future<Output = Result<(), JsValue>> + 'static {
-    let request = Request {
-        data: [JsValuePreserved::new(request)],
-    };
+    let Request { data } = from_value(request).expect("expected request with `data` field");
+    let request = Request { data: [data] };
     let task = task_spawner.spawn_batch_scoped(request, |recv| {
         future_to_promise(async move {
             let mut sender = sender;
