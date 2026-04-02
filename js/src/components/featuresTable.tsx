d4e607228f9d31fbc6e68696dc7af352cd5f8388
alex-dsci <alex-dsci@github.com>
2026-04-01 20:52:41 -0400

    add precision limit

--- js/src/components/featuresTable.tsx
+++ js/src/components/featuresTable.tsx
@@ -1,10 +1,17 @@
+import type { ColumnDef } from "@tanstack/react-table";
 import DataTable from "./dataTable";
 import type { Featurized } from "@/features/featurize/types";
+import type { AcceptedData } from "node_modules/export-to-csv/output/lib/types";
 
 
 export function FeaturesTable(props: { data: Record<string, Featurized> }) {
-  const columns = Object.keys(props.data).map((featureID) => ({
+  const columns: ColumnDef<Record<string, AcceptedData>>[] = Object.keys(props.data).map((featureID) => ({
     accessorKey: featureID,
+    cell: ({ getValue }) => (
+      <span className="block truncate">
+        {getValue<number>().toPrecision(4)}
+      </span>
+    ),
   }))
   const data = [Object.fromEntries(Object.entries(props.data).map(([featureID, featurized]) => [
     featureID, featurized.case === "ok" ? featurized.value : featurized.value.reason
