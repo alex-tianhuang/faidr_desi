c98f0d4647742bf26d86f6f380b1cbcb464d1f9c
alex-dsci <alex-dsci@github.com>
2026-04-11 16:50:09 -0400

    change featurize test to include multiple sequences

--- js/tests/backend/featurize.spec.ts
+++ js/tests/backend/featurize.spec.ts
@@ -1,7 +1,7 @@
 import { SEQUENCE_VALIDATION_PARAMETERS } from "@/lib/consts";
 import FEATURE_CONFIGURATION from "./data/kl_feature_configuration";
 import { test, expect } from "./fixtures";
-import TEST_SEQUENCE from "./data/featurize/test_sequence";
+import TEST_SEQUENCES from "./data/featurize/test_sequences";
 import EXPECTED_FEATURE_VALUES from "./data/featurize/expected_feature_values";
 
 test("features can reproduce old implementation", async ({ page }) => {
@@ -10,11 +10,11 @@ test("features can reproduce old implementation", async ({ page }) => {
       const {
         FEATURE_CONFIGURATION,
         SEQUENCE_VALIDATION_PARAMETERS,
-        TARGET_SEQUENCE,
+        TARGET_SEQUENCES,
       } = imports;
       const request = {
         endpoint: "featurize",
-        sequences: [TARGET_SEQUENCE],
+        sequences: TARGET_SEQUENCES,
         featureConfiguration: FEATURE_CONFIGURATION,
         sequenceValidationSettings: SEQUENCE_VALIDATION_PARAMETERS,
         statisticsIncluded: false,
@@ -24,7 +24,7 @@ test("features can reproduce old implementation", async ({ page }) => {
     {
       FEATURE_CONFIGURATION,
       SEQUENCE_VALIDATION_PARAMETERS,
-      TARGET_SEQUENCE: TEST_SEQUENCE,
+      TARGET_SEQUENCES: TEST_SEQUENCES,
     },
   );
   expect(result.error).toBeNull();
@@ -36,17 +36,22 @@ test("features can reproduce old implementation", async ({ page }) => {
     sequenceValidationErrors: {},
     statisticsIncluded: false,
   });
-  expect(result.yielded.length).toEqual(2);
-  const { phase: progress, sequenceByFeatureMatrix } = result.yielded[1] as any;
-  expect(progress).toEqual("progress");
-  expect(sequenceByFeatureMatrix).toHaveProperty("0");
-  const initialFeaturized = sequenceByFeatureMatrix["0"];
-  for (const [featureID, result] of Object.entries(initialFeaturized)) {
-    const expectedValue = (EXPECTED_FEATURE_VALUES as Record<string, number>)[
-      featureID
-    ];
-    const { case: ok_or_err, value } = result as any;
-    expect(ok_or_err).toEqual("ok");
-    expect(value).toBeCloseTo(expectedValue);
+  const results = new Array(EXPECTED_FEATURE_VALUES.length);
+  for (const yielded of result.yielded.slice(1)) {
+    const { phase: progress, sequenceByFeatureMatrix } = yielded as any;
+    expect(progress).toEqual("progress");
+    for (const [index, featureVector] of Object.entries(sequenceByFeatureMatrix)) {
+      results[Number(index)] = featureVector;
+    }
   }
+  for (const [idx, featureVector] of results.entries()) {
+    for (const [featureID, result] of Object.entries(featureVector)) {
+      const expectedValue = (EXPECTED_FEATURE_VALUES[idx] as Record<string, number>)[
+        featureID
+      ];
+      const { case: ok_or_err, value } = result as any;
+      expect(ok_or_err).toEqual("ok");
+      expect(value).toBeCloseTo(expectedValue);
+    }
+  }  
 });
