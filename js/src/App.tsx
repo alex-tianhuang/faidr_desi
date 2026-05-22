6ded4a616b93210da2c21afff070f2f029fddb9f
alex-dsci <alex-dsci@github.com>
2026-05-22 11:56:48 -0400

    refactor out tool to own file

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -1,30 +1,17 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ToolButtons from "./page/toolButtons";
-import GenerateMimicArea from "./features/generateMimic";
-import {
-  FEATURE_CONFIGURATION,
-  FEATURE_MINIMUMS,
-  FEATURE_WEIGHTS,
-  type IDRome,
-} from "./lib/consts";
-import FeaturizeArea from "./features/featurize";
-import GenerateKOArea from "./features/generateKO";
+import { type IDRome } from "./lib/consts";
 import Banner from "./page/banner";
 import Preamble from "./page/preamble";
 import Footer from "./page/footer";
 import SequenceInput from "./page/sequenceInput";
+import Tool from "./page/tools";
 
 export default function App() {
   const [sequence, setSequence] = useState<string | null>(null);
   const [tool, setTool] = useState<"mimic" | "ko" | "feats" | null>(null);
   const [idrome, setIdrome] = useState<IDRome>("human");
   const [freezeInputs, setFreezeInputs] = useState<boolean>(false);
-  // guard because sometimes `requestStarted` gets stuck
-  useEffect(() => {
-    if (sequence === null || tool === null) {
-      setFreezeInputs(false);
-    }
-  }, [sequence, tool]);
   return (
     <div className="flex flex-col h-screen w-screen">
       <Banner />
@@ -38,61 +25,14 @@ export default function App() {
           toolState={[tool, setTool]}
           disabled={freezeInputs || sequence === null}
         ></ToolButtons>
-        <ToolArea
+        <Tool
           sequence={sequence}
           tool={tool}
           freezeInputsState={[freezeInputs, setFreezeInputs]}
           idromeState={[idrome, setIdrome]}
-        ></ToolArea>
+        ></Tool>
       </div>
       <Footer />
     </div>
   );
-}
-/** Displays the chosen tool if there is a sequence and tool. */
-function ToolArea(props: {
-  sequence: string | null;
-  tool: "mimic" | "ko" | "feats" | null;
-  freezeInputsState: [boolean, (_: boolean) => void];
-  idromeState: [IDRome, (_: IDRome) => void];
-}) {
-  const {
-    sequence,
-    tool,
-    freezeInputsState,
-    idromeState: [idrome, setIdrome],
-  } = props;
-  if (tool === null || sequence === null) return null;
-  if (tool === "mimic") {
-    return (
-      <GenerateMimicArea
-        sequence={sequence}
-        featureConfiguration={FEATURE_CONFIGURATION}
-        featureWeights={FEATURE_WEIGHTS[idrome]}
-        requestStartedState={freezeInputsState}
-        idromeState={[idrome, setIdrome]}
-      ></GenerateMimicArea>
-    );
-  }
-  if (tool === "ko") {
-    return (
-      <GenerateKOArea
-        sequence={sequence}
-        featureConfiguration={FEATURE_CONFIGURATION}
-        featureWeights={FEATURE_WEIGHTS[idrome]}
-        requestStartedState={freezeInputsState}
-        KOFeatureTargets={FEATURE_MINIMUMS[idrome]}
-        idromeState={[idrome, setIdrome]}
-      ></GenerateKOArea>
-    );
-  }
-  if (tool === "feats") {
-    return (
-      <FeaturizeArea
-        sequence={sequence}
-        featureConfiguration={FEATURE_CONFIGURATION}
-        idromeState={[idrome, setIdrome]}
-      ></FeaturizeArea>
-    );
-  }
 }
---
+++ js/src/page/tools/index.tsx
@@ -0,0 +1,55 @@
+import GenerateMimicArea from "@/features/generateMimic";
+import {
+  FEATURE_CONFIGURATION,
+  FEATURE_MINIMUMS,
+  FEATURE_WEIGHTS,
+  type IDRome,
+} from "@/lib/consts";
+import FeaturizeArea from "@/features/featurize";
+import GenerateKOArea from "@/features/generateKO";
+export default function Tool(props: {
+  sequence: string | null;
+  tool: "mimic" | "ko" | "feats" | null;
+  freezeInputsState: [boolean, (_: boolean) => void];
+  idromeState: [IDRome, (_: IDRome) => void];
+}) {
+  const {
+    sequence,
+    tool,
+    freezeInputsState,
+    idromeState: [idrome, setIdrome],
+  } = props;
+  if (tool === null || sequence === null) return null;
+  if (tool === "mimic") {
+    return (
+      <GenerateMimicArea
+        sequence={sequence}
+        featureConfiguration={FEATURE_CONFIGURATION}
+        featureWeights={FEATURE_WEIGHTS[idrome]}
+        requestStartedState={freezeInputsState}
+        idromeState={[idrome, setIdrome]}
+      ></GenerateMimicArea>
+    );
+  }
+  if (tool === "ko") {
+    return (
+      <GenerateKOArea
+        sequence={sequence}
+        featureConfiguration={FEATURE_CONFIGURATION}
+        featureWeights={FEATURE_WEIGHTS[idrome]}
+        requestStartedState={freezeInputsState}
+        KOFeatureTargets={FEATURE_MINIMUMS[idrome]}
+        idromeState={[idrome, setIdrome]}
+      ></GenerateKOArea>
+    );
+  }
+  if (tool === "feats") {
+    return (
+      <FeaturizeArea
+        sequence={sequence}
+        featureConfiguration={FEATURE_CONFIGURATION}
+        idromeState={[idrome, setIdrome]}
+      ></FeaturizeArea>
+    );
+  }
+}
