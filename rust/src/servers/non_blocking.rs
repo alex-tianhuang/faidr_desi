01480dc3d3625c02b4664a886201bbc6d6f698c4
alex-dsci <alex-dsci@github.com>
2026-05-22 10:48:00 -0400

    removed panic from forward_request

--- rust/src/servers/non_blocking.rs
+++ rust/src/servers/non_blocking.rs
@@ -1,4 +1,5 @@
 use super::is_hup_error;
+use crate::adapters::JsValuePreserved;
 use crate::{Receiver, Sender, TaskSpawner, datatypes::webworker_messages::get_connection_id};
 use crate::{
     ResponsePayloadWithWorkerID,
@@ -74,7 +75,12 @@ pub async fn non_blocking_server(
                 .into());
             }
         };
-        new_task!(forward_request(received, &task_spawner, sender));
+
+        let Ok(Request { data }) = from_value(received) else {
+            new_task!(sender.send_error("internal error occurred, expected `data` field"));
+            continue
+        };
+        new_task!(forward_request(data, &task_spawner, sender));
     }
 }
 
@@ -82,15 +88,11 @@ pub async fn non_blocking_server(
 ///
 /// All responses from the given job are propagated
 /// back to the [`SenderHandle`].
-///
-/// This function panics if the `request` does not have a `data` field.
-/// Use [`crate::datatypes::webworker_messages::non_blocking::request_is_forwardable`] to validate this.
 pub fn forward_request(
-    request: JsValue,
+    data: JsValuePreserved,
     task_spawner: &TaskSpawner,
     sender: SenderHandle,
 ) -> impl Future<Output = Result<(), JsValue>> + 'static {
-    let Request { data } = from_value(request).expect("expected request with `data` field");
     let request = Request { data: [data] };
     let task = task_spawner.spawn_batch_scoped(request, |recv| {
         future_to_promise(async move {
