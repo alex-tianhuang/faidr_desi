b5bcc0c071ce79c92256b8b44c4f2963b45dd446
alex-dsci <alex-dsci@github.com>
2026-05-22 10:52:34 -0400

    move task_spawner out of subfolder

--- rust/src/adapters/task_spawner.rs
+++ rust/src/adapters/task_spawner.rs
@@ -2,15 +2,14 @@
 use crate::{
     adapters::{
         JsTypedFuture, JsValuePreserved, js_typed_fut::JsTypedFutureError, serialize,
-        task_spawner::scoped_handle::ScopedHandle,
     },
     datatypes::{Request, Response, webworker_messages::common::ResponsePayloadWithWorkerID},
 };
+use futures::FutureExt;
 use serde::{Deserialize, Serialize};
 use tsify::Tsify;
 use wasm_bindgen::{UnwrapThrowExt, prelude::Closure};
 use web_sys::js_sys::{Function, Promise};
-mod scoped_handle;
 
 /// Wraps a `spawnBatch` handler which communicates with sub-workers
 /// to do (primarily blocking) tasks.
@@ -80,3 +79,30 @@ impl TaskSpawner {
         }
     }
 }
+
+/// Struct used by [`crate::adapters::TaskSpawner`] to represent an ongoing
+/// connection in the [`crate::adapters::TaskSpawner::spawn_batch_scoped`] function.
+///
+/// This struct should not be dropped before it is polled to
+/// completion as it owns a closure which JS will call whenever it
+/// feels like, as long as the promise is unresolved.
+struct ScopedHandle {
+    /// Closure that JS can call for as long as the underlying
+    /// promise is unresolved. It is only kept to prevent it
+    /// from deallocating before JS calls it.
+    #[allow(unused)]
+    pub(super) body_closure: Closure<dyn FnMut(Function) -> Promise>,
+    /// Inner promise returned by JS handler `spawnBatch`
+    /// that is actually being awaited.
+    pub(super) spawn_batch_promise: JsTypedFuture<()>,
+}
+impl Future for ScopedHandle {
+    type Output = Result<(), JsTypedFutureError>;
+    fn poll(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        self.get_mut().spawn_batch_promise.poll_unpin(cx)
+    }
+}
+
