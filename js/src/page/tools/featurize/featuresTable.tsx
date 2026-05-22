55cca30b439257158238d1b65a5cc581fbddbb38
alex-dsci <alex-dsci@github.com>
2026-05-22 13:23:51 -0400

    refactor out featurestable

--- js/src/page/tools/featurize/featuresTable.tsx
+++ js/src/page/tools/featurize/featuresTable.tsx
@@ -1,15 +1,21 @@
 import type { ColumnDef } from "@tanstack/react-table";
-import DataTable from "../../../components/dataTable";
+import DataTable from "@/components/dataTable";
 import type { Featurized } from "@/types/featurize";
 import type { AcceptedData } from "@/../node_modules/export-to-csv/output/lib/types";
+import type { IDRome } from "@/lib/consts";
 
 export function FeaturesTable(props: {
-  data: Record<string, Featurized>;
-  downloadButtonText: string;
+  featurized: Record<string, Featurized>;
+  postProcessing: IDRome | "none";
 }) {
-  const { downloadButtonText } = props;
+  const { featurized, postProcessing } = props;
+
+  const postProcessingDescription =
+    postProcessing !== "none"
+      ? `${postProcessing} IDRome Z-scores`
+      : "raw features";
   const columns: ColumnDef<Record<string, AcceptedData>>[] = Object.keys(
-    props.data,
+    featurized,
   ).map((featureID) => ({
     accessorKey: featureID,
     cell: ({ getValue }) => {
@@ -17,20 +23,31 @@ export function FeaturesTable(props: {
       return typeof value === "number" ? value.toPrecision(4) : value;
     },
   }));
-  const data = [
+  const tableData = [
     Object.fromEntries(
-      Object.entries(props.data).map(([featureID, featurized]) => [
+      Object.entries(featurized).map(([featureID, featurized]) => [
         featureID,
         featurized.case === "ok" ? featurized.value : featurized.value.reason,
       ]),
     ),
   ];
   return (
-    <DataTable
-      columns={columns}
-      data={data}
-      suggestedFilename="features.csv"
-      downloadButtonText={downloadButtonText}
-    ></DataTable>
+    <div className="flex flex-col gap-2 p-4 border rounded-md border-primary">
+      <p className="text-xl font-bold text-center">
+        Computing sequence features
+      </p>
+      <p className="text-center text-muted-foreground">
+        View and download a CSV of your sequence features below.
+      </p>
+      <DataTable
+        columns={columns}
+        data={tableData}
+        suggestedFilename="features.csv"
+        downloadButtonText={`Download as CSV (${postProcessingDescription})`}
+      ></DataTable>
+      <p className="text-center text-muted-foreground">
+        Scroll horizontally in the table above to see more sequence features!
+      </p>
+    </div>
   );
 }
--- js/src/page/tools/featurize/index.tsx
+++ js/src/page/tools/featurize/index.tsx
@@ -1,13 +1,11 @@
 import useFeaturizeEndpoint from "@/backend/apis/featurize";
 import { FeaturesTable } from "@/page/tools/featurize/featuresTable";
-import { Alert } from "@/components/ui/alert";
 import { useMemo, useState } from "react";
 import { type IDRome } from "@/lib/consts";
-import type { Featurized } from "@/types/featurize";
 import Loading from "@/components/loading";
-import { Toggle } from "@/components/ui/toggle";
 import { UnexpectedError } from "@/components/errors";
 import featuresToIDRomeZscores from "@/lib/utils";
+import ZScoreButtons from "./zscoreButtons";
 
 export default function FeaturizeArea(props: {
   sequence: string;
@@ -29,106 +27,38 @@ export default function FeaturizeArea(props: {
         : featurizedRaw,
     [featurizedRaw, postProcessing],
   );
+  if (featurizationError) {
+    return (
+      <UnexpectedError
+        while="computing sequence features of your input sequence"
+        error={featurizationError}
+      />
+    );
+  }
+  if (!featurized) {
+    return (
+      <div className="flex flex-col gap-2 p-4 border rounded-md border-primary">
+        <Loading>Computing sequence features...</Loading>
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col gap-2">
-      {featurizationError ? (
-        <UnexpectedError
-          while="computing sequence features of your input sequence"
-          error={featurizationError}
-        ></UnexpectedError>
-      ) : (
-        <FeaturizeResultsArea
-          featurized={featurized}
-          postProcessingState={[
-            postProcessing,
-            (option) => {
-              setPostProcessing(option);
-              if (option === "yeast" || option === "human") {
-                setIdrome(option);
-              }
-            },
-          ]}
-        ></FeaturizeResultsArea>
-      )}
+      <FeaturesTable
+        postProcessing={postProcessing}
+        featurized={featurized}
+      ></FeaturesTable>
+      <ZScoreButtons
+        postProcessingState={[
+          postProcessing,
+          (option) => {
+            setPostProcessing(option);
+            if (option === "yeast" || option === "human") {
+              setIdrome(option);
+            }
+          },
+        ]}
+      ></ZScoreButtons>
     </div>
-  );
-}
-function FeaturizeResultsArea(props: {
-  featurized: Record<string, Featurized> | null;
-  postProcessingState: [IDRome | "none", (_: IDRome | "none") => void];
-}) {
-  const {
-    postProcessingState: [postProcessing, setPostProcessing],
-    featurized,
-  } = props;
-  const postProcessingDescription =
-    postProcessing !== "none"
-      ? `${postProcessing} IDRome Z-scores`
-      : "raw features";
-  const optionDescription = (option: typeof postProcessing) =>
-    option !== "none"
-      ? `Z-score features (${option} IDRome)`
-      : "Raw feature values";
-  return (
-    <>
-      <div className="flex flex-col gap-2 p-4 border rounded-md border-primary">
-        <p className="text-xl font-bold text-center">
-          Computing sequence features
-        </p>
-        <p className="text-center text-muted-foreground">
-          View and download a CSV of your sequence features below.
-        </p>
-        {featurized ? (
-          <>
-            <FeaturesTable
-              data={featurized}
-              downloadButtonText={`Download as CSV (${postProcessingDescription})`}
-            ></FeaturesTable>
-            <p className="text-center text-muted-foreground">
-              Scroll horizontally in the table above to see more sequence
-              features!
-            </p>
-          </>
-        ) : (
-          <Loading>Computing sequence features...</Loading>
-        )}
-      </div>
-      {featurized && (
-        <div className="flex flex-col gap-2 p-4 border rounded-md border-primary">
-          <p className="text-md font-bold underline">
-            Switch between raw features and Z-scores
-          </p>
-          <p className="text-justify text-muted-foreground">
-            Click one of the buttons below to change how features are reported
-            in the table above.
-          </p>
-          <div className="flex flex-row flex-wrap items-center gap-2">
-            {(["human", "yeast", "none"] as const).map((option) => {
-              const pressed = option === postProcessing;
-              return (
-                <Toggle
-                  className="flex-1 min-w-fit"
-                  key={option}
-                  pressed={pressed}
-                  onPressedChange={(pressed) => {
-                    if (pressed) {
-                      setPostProcessing(option);
-                    }
-                  }}
-                >
-                  {optionDescription(option)}
-                </Toggle>
-              );
-            })}
-            <Alert>
-              <span>
-                Viewing sequence features as{" "}
-                <span className="underline">{postProcessingDescription}</span>
-              </span>
-            </Alert>
-          </div>
-        </div>
-      )}
-    </>
   );
 }
