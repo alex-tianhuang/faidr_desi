d2125f2c6d60e4136e6b23224b7b0741716c4635
alex-dsci <alex-dsci@github.com>
2026-08-02 18:00:38 -0400

    refactor out datatable to two different use cases separately

--- js/src/components/designIterationsTable.tsx
+++ js/src/components/designIterationsTable.tsx
@@ -1,6 +1,21 @@
 import type { DesignIteration } from "@/types/common";
-import type { ColumnDef } from "@tanstack/react-table";
-import DataTable from "./dataTable";
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  type ColumnDef,
+} from "@tanstack/react-table";
+import { asString, generateCsv } from "export-to-csv";
+import { Button } from "./ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
+import { saveFile } from "@/lib/utils";
 
 const COLUMNS: ColumnDef<DesignIteration>[] = [
   {
@@ -25,8 +40,39 @@ const COLUMNS: ColumnDef<DesignIteration>[] = [
   },
 ];
 
-export function DesignIterationsTable(props: { iterations: DesignIteration[] }) {
+export function DesignIterationsTable(props: {
+  iterations: DesignIteration[];
+}) {
   const { iterations } = props;
+  const table = useReactTable({
+    data: iterations,
+    columns: COLUMNS,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  const handleExport = () => {
+    const headers = table.getVisibleFlatColumns().reduce(
+      (acc, col) => {
+        const header = col.columnDef.header;
+        if (typeof header === "string") {
+          acc[col.id] = header;
+        }
+        return acc;
+      },
+      {} as Record<string, string>,
+    );
+    const rows = table.getCoreRowModel().rows;
+    const rowData = rows.map((row) =>
+      Object.fromEntries(
+        Object.entries(row.original).map(([key, val]) => [
+          headers[key] ?? key,
+          val,
+        ]),
+      ),
+    );
+    const csv = asString(generateCsv({ useKeysAsHeaders: true })(rowData));
+    saveFile(csv, "design_iterations.csv");
+  };
   return (
     <details className="text-sm">
       <summary className="cursor-pointer text-muted-foreground hover:text-foreground">
@@ -34,14 +80,56 @@ export function DesignIterationsTable(props: { iterations: DesignIteration[] }) 
           ? `Show all iterations (${iterations.length - 1})`
           : "Iterations will appear here"}
       </summary>
-      {iterations.length > 0 ? <div className="mt-2">
-        <DataTable
-          columns={COLUMNS}
-          data={iterations}
-          suggestedFilename="design_iterations.csv"
-          downloadButtonText="Download as CSV"
-        ></DataTable>
-      </div> : <div className="mt-2 p-4 text-center border border-input rounded-md">Iterations will appear here (no iterations yet)</div>}
+      {iterations.length > 0 ? (
+        <div className="mt-2">
+          <div className="flex flex-col overflow-hidden border rounded-md items-end p-4 gap-2 border-input">
+            <Button className="w-full" onClick={handleExport}>
+              {"Download as CSV"}
+            </Button>
+            <Table>
+              <TableHeader>
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => {
+                      return (
+                        <TableHead key={header.id}>
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(
+                                header.column.columnDef.header,
+                                header.getContext(),
+                              )}
+                        </TableHead>
+                      );
+                    })}
+                  </TableRow>
+                ))}
+              </TableHeader>
+              <TableBody>
+                {table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      ) : (
+        <div className="mt-2 p-4 text-center border border-input rounded-md">
+          Iterations will appear here (no iterations yet)
+        </div>
+      )}
     </details>
   );
 }
--- js/src/page/tools/featurize/featuresTable.tsx
+++ js/src/page/tools/featurize/featuresTable.tsx
@@ -1,25 +1,78 @@
-import type { ColumnDef } from "@tanstack/react-table";
-import DataTable from "@/components/dataTable";
-import type { AcceptedData } from "@/../node_modules/export-to-csv/output/lib/types";
 import { featuresToTableData } from "@/lib/utils";
+import {
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  useReactTable,
+  type ColumnDef,
+  type SortingState,
+} from "@tanstack/react-table";
+import { asString, generateCsv } from "export-to-csv";
+import type { AcceptedData } from "@/../node_modules/export-to-csv/output/lib/types";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { saveFile } from "@/lib/utils";
+import React from "react";
+
+const COLUMNS: ColumnDef<Record<string, AcceptedData>>[] = [
+  "Feature Name",
+  "Raw Value",
+  "Human Z-Score",
+  "Yeast Z-Score",
+].map((column) => ({
+  accessorKey: column,
+  cell: ({ getValue }) => {
+    const value = getValue<number | string>();
+    return typeof value === "number" ? value.toPrecision(4) : value;
+  },
+}));
 
 export function FeaturesTable(props: {
   featurized: ReturnType<typeof featuresToTableData>;
 }) {
   const { featurized } = props;
 
-  const columns: ColumnDef<Record<string, AcceptedData>>[] = [
-    "Feature Name",
-    "Raw Value",
-    "Human Z-Score",
-    "Yeast Z-Score",
-  ].map((column) => ({
-    accessorKey: column,
-    cell: ({ getValue }) => {
-      const value = getValue<number | string>();
-      return typeof value === "number" ? value.toPrecision(4) : value;
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const table = useReactTable({
+    data: featurized,
+    columns: COLUMNS,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
     },
-  }));
+    onSortingChange: setSorting,
+  });
+  const handleExport = () => {
+    const headers = table.getVisibleFlatColumns().reduce(
+      (acc, col) => {
+        const header = col.columnDef.header;
+        if (typeof header === "string") {
+          acc[col.id] = header;
+        }
+        return acc;
+      },
+      {} as Record<string, string>,
+    );
+    const rows = table.getSortedRowModel().rows;
+    const rowData = rows.map((row) =>
+      Object.fromEntries(
+        Object.entries(row.original).map(([key, val]) => [
+          headers[key] ?? key,
+          val,
+        ]),
+      ),
+    );
+    const csv = asString(generateCsv({ useKeysAsHeaders: true })(rowData));
+    saveFile(csv, "features.csv");
+  };
   return (
     <div className="flex flex-col gap-2 p-4 border rounded-md border-primary max-h-100 overflow-auto">
       <p className="text-xl font-bold text-center">
@@ -28,12 +81,81 @@ export function FeaturesTable(props: {
       <p className="text-center text-muted-foreground">
         View and download a CSV of your sequence features below.
       </p>
-      <DataTable
-        columns={columns}
-        data={featurized}
-        suggestedFilename="features.csv"
-        downloadButtonText="Download as CSV"
-      ></DataTable>
+      <div className="flex flex-col overflow-hidden border rounded-md items-end p-4 gap-2 border-input">
+        <Button className="w-full" onClick={handleExport}>
+          {"Download as CSV"}
+        </Button>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder ? null : (
+                        <div
+                          className={
+                            header.column.getCanSort()
+                              ? "cursor-pointer select-none"
+                              : ""
+                          }
+                          onClick={header.column.getToggleSortingHandler()}
+                          title={
+                            header.column.getCanSort()
+                              ? header.column.getNextSortingOrder() === "asc"
+                                ? "Sort ascending"
+                                : header.column.getNextSortingOrder() === "desc"
+                                  ? "Sort descending"
+                                  : "Clear sort"
+                              : undefined
+                          }
+                        >
+                          {flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                          {{
+                            asc: " 🔼",
+                            desc: " 🔽",
+                          }[header.column.getIsSorted() as string] ?? null}
+                        </div>
+                      )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={COLUMNS.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
       <p className="text-center text-muted-foreground">
         Scroll vertically in the table above to see more sequence features!
       </p>
