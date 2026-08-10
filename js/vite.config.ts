944a2988a6aefec127004f1d939413f95d945813
alex-dsci <alex-dsci@github.com>
2026-08-10 17:53:30 -0400

    Migrated to preact correctly

--- js/vite.config.ts
+++ js/vite.config.ts
@@ -1,12 +1,12 @@
 import path from "path"
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import preact from "@preact/preset-vite"
 import tailwindcss from '@tailwindcss/vite'
 import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [visualizer(), react(), tailwindcss()],
+  plugins: [visualizer(), preact(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "./src"),
