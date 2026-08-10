505e0113ac3ee18635df5a9c6d467a76f6916b6a
alex-dsci <alex-dsci@github.com>
2026-08-10 08:16:52 -0400

    fix dep warning __dirname change to import.meta.dirname

--- js/vite.config.ts
+++ js/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [visualizer(), react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(import.meta.dirname, "./src"),
     },
   },
   base: "/idr/faidr-desi"
