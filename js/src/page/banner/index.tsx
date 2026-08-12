62a12005bb9c02736483d82d8e9148ebb1b8a403
alex-dsci <alex-dsci@github.com>
2026-08-12 10:13:16 -0400

    set title font-serif

--- js/src/page/banner/index.tsx
+++ js/src/page/banner/index.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from "./themeToggle";
 export default function Banner() {
   return (
     <div className="flex flex-col sticky w-full top-0 px-5 pt-3 pb-2 shadow border-b bg-background gap-2 z-50 border-foreground">
-      <div className="text-center font-extrabold text-3xl underline">
+      <div className="text-center font-extrabold text-3xl underline font-serif">
         FAIDR-Desi
         <div className="absolute top-2.5 right-4">
           <ThemeToggle/>
