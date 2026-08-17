5d06c89054eef032350380bccdca218d9eea011c
alex-dsci <alex-dsci@github.com>
2026-08-17 15:18:30 -0400

    text edit jdfk get 127 features -> compute 127 features

--- js/src/page/toolButtons/index.tsx
+++ js/src/page/toolButtons/index.tsx
@@ -24,7 +24,7 @@ const TOOL_INFO = [
   },
   {
     propKey: "feats",
-    label: `Get ${NUM_FEATURES} sequence features`,
+    label: `Compute ${NUM_FEATURES} sequence features`,
     helpTitle: "Computing features",
     helpDiv: <FeaturizeHelp />,
   },
